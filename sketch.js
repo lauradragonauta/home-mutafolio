cafec284bc349ced83605701cbb7077c6609a4c2
dragonauta <laurarosadlp@gmail.com>
2025-05-07 13:28:33 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let particles = [];
-const numParticles = 800;
+const numParticles = 5000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -9,10 +9,7 @@ function setup() {
 }
 
 function draw() {
-  clear;
-  orbitControl(); // permite rotar suavemente con el mouse
-  rotateX(frameCount * 0.001);
-  rotateY(frameCount * 0.001);
+  clear(); // fondo transparente ideal para Readymag
   
   for (let p of particles) {
     p.update();
@@ -22,39 +19,45 @@ function draw() {
 
 class Particle {
   constructor() {
-    this.pos = createVector(random(-width / 2, width / 2), random(-height / 2, height / 2), random(-300, 300));
-    this.vel = p5.Vector.random3D().mult(0.5);
+    this.pos = createVector(
+      random(-width / 2, width / 2),
+      random(-height / 2, height / 2),
+      0 // todas en el mismo plano Z
+    );
+    this.vel = p5.Vector.random2D().mult(0.5); // solo movimiento 2D
     this.acc = createVector(0, 0, 0);
     this.size = random(2, 5);
     this.gray = random(50, 200);
   }
 
   update() {
-    let mouse = createVector((mouseX - width / 2), (mouseY - height / 2), 0);
+    let mouse = createVector(mouseX - width / 2, mouseY - height / 2, 0);
     let dir = p5.Vector.sub(mouse, this.pos);
     let d = dir.mag();
-    let mouseRadius = 200; // área de influencia
-  
+    let mouseRadius = 200;
+
     if (d < mouseRadius) {
       dir.normalize();
-      let strength = map(d, 0, mouseRadius, 1.5, 0.1); // más fuerza cerca, menos lejos
+      let strength = map(d, 0, mouseRadius, 1.5, 0.1);
       dir.mult(strength);
       this.acc = dir;
     } else {
-      this.acc.mult(0); // sin influencia
+      let angle = noise(this.pos.x * 0.002, this.pos.y * 0.002, frameCount * 0.002) * TWO_PI;
+      let flow = createVector(cos(angle), sin(angle), 0); // sin Z
+      flow.mult(0.1);
+      this.acc = flow;
     }
-  
+
     this.vel.add(this.acc);
     this.vel.limit(2);
     this.pos.add(this.vel);
   }
-  
 
   display() {
     push();
     translate(this.pos.x, this.pos.y, this.pos.z);
     noStroke();
-    fill(this.gray);
+    fill(this.gray, 100); // semitransparente
     sphere(this.size);
     pop();
   }
@@ -62,4 +65,11 @@ class Particle {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  particles = [];
+  for (let i = 0; i < numParticles; i++) {
+    particles.push(new Particle());
+  }
 }
